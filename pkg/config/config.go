88d7ac05600b5e1c83e4340229b2d6625d183803
Joshua Ward <j2udevelopment@gmail.com>
2021-06-28 18:54:01 -0400

    abstracting config into config package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,23 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// ConfigFile struct used to make passing around config files easier
+type ConfigFile struct {
+	Path      string
+	Extension string
+	FileName  string
+	Override  string
+}
+
 // Config struct used to unmarshal yaml kruise configuration
 type Config struct {
 	Deploy map[string][]DeployConfig `mapstructure:"deploy"`
@@ -24,4 +42,39 @@ type HelmConfig struct {
 	Values      []string
 	Args        []string
 	ExtraArgs   []string
+}
+
+var config Config
+
+// InitConfig initializes default config
+func InitConfig() {
+	home, err := homedir.Dir()
+	cobra.CheckErr(err)
+	configFile := ConfigFile{
+		Path:      home,
+		Extension: "yaml",
+		FileName:  ".kruise",
+	}
+	InitCustomConfig(configFile)
+}
+
+// InitCustomConfig reads in a ConfigFile that is passed to viper
+func InitCustomConfig(configFile ConfigFile) {
+	if configFile.Override != "" {
+		// Use config file from override
+		viper.SetConfigFile(configFile.Override)
+	} else {
+		viper.AddConfigPath(configFile.Path)
+		viper.SetConfigType(configFile.Extension)
+		viper.SetConfigName(configFile.FileName)
+	}
+	viper.AutomaticEnv() // read in environment variables that match
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("Unable to decode config into struct, %v", err)
+	}
 }
