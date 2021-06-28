022d0e48eaf490337b49bb6cf21063c42cd9cfd5
Joshua Ward <j2udevelopment@gmail.com>
2021-06-28 19:04:48 -0400

    updating helm testing

--- pkg/helm/helm_test.go
+++ pkg/helm/helm_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConstructHelmChart(t *testing.T) {
+func TestConstructHelmInstallCommand(t *testing.T) {
 	helmCommands := []string{
 		"helm upgrade -i jaeger jaeger/jaeger --namespace jaeger --create-namespace",
 		"helm upgrade -i kafka kafka/kafka --namespace kafka --create-namespace",
@@ -31,7 +31,33 @@ func TestConstructHelmChart(t *testing.T) {
 	for i, config := range helmConfig {
 		ConstructHelmCommand(&config)
 		DefineInstallArgs(&config)
-		// fmt.Println(config)
+		assert.Equal(t, helmCommands[i], strings.Join(config.Args, " "), "helmCommand did not match parsed helmConfig")
+	}
+}
+
+func TestConstructHelmUninstallCommand(t *testing.T) {
+	helmCommands := []string{
+		"helm uninstall jaeger --namespace jaeger",
+		"helm uninstall kafka --namespace kafka",
+		"helm uninstall mongodb --namespace mongodb",
+		"helm uninstall mysql --namespace mysql",
+		"helm uninstall postgresql --namespace postgresql",
+		"helm uninstall prom-op --namespace prom-op",
+		"helm uninstall test!@#$release --namespace test --dry-run",
+	}
+	helmConfig := []c.HelmConfig{
+		{ReleaseName: "jaeger", ChartPath: "jaeger/jaeger", Namespace: "jaeger"},
+		{ReleaseName: "kafka", ChartPath: "kafka/kafka", Namespace: "kafka"},
+		{ReleaseName: "mongodb", ChartPath: "mongodb/mongodb", Namespace: "mongodb"},
+		{ReleaseName: "mysql", ChartPath: "mysql/mysql", Namespace: "mysql"},
+		{ReleaseName: "postgresql", ChartPath: "postgresql/postgresql", Namespace: "postgresql"},
+		{ReleaseName: "prom-op", ChartPath: "prom-op/prom-op", Namespace: "prom-op"},
+		{ReleaseName: "test!@#$release", ChartPath: "test!@#$/test!@#$", Namespace: "test", Version: "7.7.7", ExtraArgs: []string{"--dry-run"}},
+	}
+	assert.Equal(t, len(helmCommands), len(helmConfig), "The length of the helmCommands slice did not match the length of the helmConfig slice")
+	for i, config := range helmConfig {
+		ConstructHelmCommand(&config)
+		DefineUninstallArgs(&config)
 		assert.Equal(t, helmCommands[i], strings.Join(config.Args, " "), "helmCommand did not match parsed helmConfig")
 	}
 }
