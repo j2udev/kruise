4fe7b23dc19a543d9bc4059120f92ea443f57453
hak33m16 <hak33m16@users.noreply.github.com>
2021-07-03 13:12:43 -0400

    #16 update pipeline with new steps; update tests to follow proper go standards

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -2,19 +2,17 @@ package utils
 
 import (
 	"testing"
-
-	u "github.com/j2udevelopment/kruise/pkg/utils"
 )
 
 func TestContainsTruthy(t *testing.T) {
-	result := u.Contains([]string{"this", "is", "a", "test"}, "test")
+	result := Contains([]string{"this", "is", "a", "test"}, "test")
 	if result != true {
 		t.Fatalf("Expected true")
 	}
 }
 
 func TestContainsFalsy(t *testing.T) {
-	result := u.Contains([]string{"this", "is", "a", "test"}, "doesnthaveme")
+	result := Contains([]string{"this", "is", "a", "test"}, "doesnthaveme")
 	if result != false {
 		t.Fatalf("Expected false")
 	}
