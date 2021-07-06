23e7f41ed7b3bada1aeebbca8df59d2755917a26
Joshua Ward <j2udevelopment@gmail.com>
2021-07-06 09:47:20 -0400

    fixed config tests

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -6,35 +6,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnmarshalDeployConfig(t *testing.T) {
-	cfgFile := ConfigFile{"../../test/deploy", "yaml", "deploy-test-config", ""}
-	var cfg Config
-	var helmCfg HelmConfig
-	InitCustomConfig(cfgFile, cfg)
-	Decode("deploy.jaeger", &helmCfg)
-	assert.Equal(t, "test-jaeger", helmCfg.ReleaseName, "The releaseName was not unmarshalled correctly")
-	assert.Equal(t, "test/jaeger", helmCfg.ChartPath, "The chartPath was not unmarshalled correctly")
-	assert.Equal(t, "test", helmCfg.Namespace, "The namespace was not unmarshalled correctly")
-	assert.Equal(t, "0.39.5", helmCfg.Version, "The version was not unmarshalled correctly")
-	assert.Equal(t, []string{"values/values.yaml"}, helmCfg.Values, "The values were not unmarshalled correctly")
-	assert.Equal(t, []string(nil), helmCfg.Args, "The args were not unmarshalled correctly")
-	assert.Equal(t, []string{"--wait", "--dry-run"}, helmCfg.ExtraArgs, "The extraArgs were not unmarshalled correctly")
-}
-
-func TestUnmarshalDynamicDeployConfig(t *testing.T) {
-	cfgFile := ConfigFile{"../../test/deploy", "yaml", "dynamic-test-config", ""}
-	var cfg DynamicConfig
-	var helmCfg []DynamicHelmConfig
-	testCfgA := DynamicHelmConfig{
+func TestUnmarshalHelmDeployConfig(t *testing.T) {
+	cfgFile := File{"../../test", "yaml", "test-kruise-manifest", ""}
+	var cfg Manifest
+	var helmCfg []HelmDeployment
+	testCfgA := HelmDeployment{
 		Option{"jaeger", "test jaeger description"},
-		HelmConfig{"jaeger", "test/jaeger", "jaeger", "7.7.7", []string{"values/values.yaml"}, nil, []string{"--wait", "--dry-run"}},
+		HelmCommand{"jaeger", "test/jaeger", "jaeger", "7.7.7", []string{"values/values.yaml"}, nil, []string{"--wait", "--dry-run"}},
 	}
-	testCfgB := DynamicHelmConfig{
+	testCfgB := HelmDeployment{
 		Option{"kafka", "test kafka description"},
-		HelmConfig{"kafka", "test/kafka", "kafka", "8.8.8", []string{"values/values.yaml"}, nil, []string{"--dry-run"}},
+		HelmCommand{"kafka", "test/kafka", "kafka", "8.8.8", []string{"values/values.yaml"}, nil, []string{"--dry-run"}},
 	}
-	InitCustomConfig(cfgFile, cfg)
+	testCfgC := HelmDeployment{
+		Option{"mongodb, mongo", "test mongodb description"},
+		HelmCommand{"mongodb", "test/mongodb", "mongodb", "9.9.9", []string{"values/values.yaml"}, nil, []string{"--reuse-values"}},
+	}
+	Initialize(cfgFile, cfg)
 	Decode("deploy.helm", &helmCfg)
 	assert.Equal(t, testCfgA, helmCfg[0], "Test configuration did not match unmarshalled/decoded configuration")
 	assert.Equal(t, testCfgB, helmCfg[1], "Test configuration did not match unmarshalled/decoded configuration")
+	assert.Equal(t, testCfgC, helmCfg[2], "Test configuration did not match unmarshalled/decoded configuration")
+}
+
+func TestUnmarshalHelmDeleteConfig(t *testing.T) {
+	cfgFile := File{"../../test", "yaml", "test-kruise-manifest", ""}
+	var cfg Manifest
+	var helmCfg []HelmDeployment
+	testCfgA := HelmDeployment{
+		Option{"jaeger", "test jaeger description"},
+		HelmCommand{ReleaseName: "jaeger", Namespace: "jaeger"},
+	}
+	testCfgB := HelmDeployment{
+		Option{"kafka", "test kafka description"},
+		HelmCommand{ReleaseName: "kafka", Namespace: "kafka"},
+	}
+	testCfgC := HelmDeployment{
+		Option{"mongodb, mongo", "test mongodb description"},
+		HelmCommand{ReleaseName: "mongodb", Namespace: "mongodb"},
+	}
+	Initialize(cfgFile, cfg)
+	Decode("delete.helm", &helmCfg)
+	assert.Equal(t, testCfgA, helmCfg[0], "Test configuration did not match unmarshalled/decoded configuration")
+	assert.Equal(t, testCfgB, helmCfg[1], "Test configuration did not match unmarshalled/decoded configuration")
+	assert.Equal(t, testCfgC, helmCfg[2], "Test configuration did not match unmarshalled/decoded configuration")
 }
