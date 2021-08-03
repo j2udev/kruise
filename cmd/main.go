ce925507d1f5298a411702c931d2dbc7498cc088
Joshua Ward <j2udevelopment@gmail.com>
2021-08-02 21:35:13 -0400

    cleaning up main.go a bit

--- cmd/main.go
+++ cmd/main.go
@@ -14,24 +14,20 @@ func main() {
 	// ---------------------TESTING---------------------
 	// -------------------------------------------------
 
-	//TODO: Figure out why the help flag is not initializing kruise config,
-	// options, etc
-	//TODO: The help command and usage works perfectly fine
+	//TODO: Figure out why the help flag is not initializing kruise config
+	//TODO: The help command and usage work perfectly fine
 	//TODO: Abstract this kind of testing to a test file
 
 	// var cmd *cobra.Command
 	// home, _ := os.UserHomeDir()
 	// //non-working args
-	// // notWorking := []string{"deploy", "--help", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
-	// // cmd = kruise.NewCmd()
-	// // cmd.SetArgs(notWorking)
-	// // cobra.CheckErr(cmd.Execute())
+	// notWorking := []string{"deploy", "--help", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
+	// cmd = kruise.NewCmd()
+	// cmd.SetArgs(notWorking)
+	// cobra.CheckErr(cmd.Execute())
 
 	// //working args
-	// // working := []string{"help", "deploy", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
-	// // working := []string{"help", "deploy", "kafka", "-d", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
-	// // working := []string{"deploy", "blah", "-d", "kafka", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
-	// working := []string{"deploy", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
+	// working := []string{"help", "deploy", fmt.Sprintf("--config=%s", home+"/.kruise-2.yaml")}
 	// cmd = kruise.NewCmd()
 	// cmd.SetArgs(working)
 	// cobra.CheckErr(cmd.Execute())
