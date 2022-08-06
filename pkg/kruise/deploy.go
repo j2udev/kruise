4772461703b3fa0a13283f6cbabdeac6ad5cecca
Joshua Ward <joshua.l.ward@leidos.com>
2022-08-06 14:12:17 +0000

    moving the few delete functions into deploy.go

--- pkg/kruise/deploy.go
+++ pkg/kruise/deploy.go
@@ -26,6 +26,24 @@ func GetDeployOptions() Options {
 	return opts
 }
 
+// Delete determines valid deployments from args and passes the cobra Cmd
+// FlagSet to the Uninstall function
+func Delete(fs *pflag.FlagSet, args []string) {
+	d := getValidDeployments(args)
+	Uninstall(fs, d...)
+}
+
+// GetDeleteOptions aggregates delete options from all deployers
+func GetDeleteOptions() Options {
+	deps := Kfg.Manifest.Deploy.Deployments
+	var opts Options
+	for k, v := range deps {
+		args := []string{k}
+		opts = append(opts, newOption(append(args, v.Aliases...), v.Description.Delete))
+	}
+	return opts
+}
+
 // GetValidDeployArgs aggregates valid deploy arguments from all deployers
 func GetValidDeployArgs() []string {
 	args := GetDeployOptions().getValidArgs()
