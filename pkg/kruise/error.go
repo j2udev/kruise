44252144f6e87be9f4a394329a98a89c8cce7a08
Joshua Ward <joshua.l.ward@leidos.com>
2022-08-06 14:11:51 +0000

    docs for error functions

--- pkg/kruise/error.go
+++ pkg/kruise/error.go
@@ -1,11 +1,14 @@
 package kruise
 
+// Trace is used to catch non-nil errors and Log them at a trace level
 func Trace(err error) {
 	if err != nil {
 		Logger.Trace(err)
 	}
 }
 
+// Tracef is used to catch non-nil errors and Log them at a trace level with a
+// custom message
 func Tracef(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Tracef(msg, args...)
@@ -13,12 +16,15 @@ func Tracef(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Debug is used to catch non-nil errors and Log them at a debug level
 func Debug(err error) {
 	if err != nil {
 		Logger.Debug(err)
 	}
 }
 
+// Debugf is used to catch non-nil errors and Log them at a debug level with a
+// custom message
 func Debugf(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Debugf(msg, args...)
@@ -26,12 +32,15 @@ func Debugf(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Info is used to catch non-nil errors and Log them at a info level
 func Info(err error) {
 	if err != nil {
 		Logger.Info(err)
 	}
 }
 
+// Infof is used to catch non-nil errors and Log them at a info level with a
+// custom message
 func Infof(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Infof(msg, args...)
@@ -39,12 +48,15 @@ func Infof(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Warn is used to catch non-nil errors and Log them at a warn level
 func Warn(err error) {
 	if err != nil {
 		Logger.Warn(err)
 	}
 }
 
+// Warnf is used to catch non-nil errors and Log them at a warn level with a
+// custom message
 func Warnf(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Warnf(msg, args...)
@@ -52,12 +64,15 @@ func Warnf(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Error is used to catch non-nil errors and Log them at a error level
 func Error(err error) {
 	if err != nil {
 		Logger.Error(err)
 	}
 }
 
+// Errorf is used to catch non-nil errors and Log them at a error level with a
+// custom message
 func Errorf(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Errorf(msg, args...)
@@ -65,12 +80,19 @@ func Errorf(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Fatal is used to catch non-nil errors and Log them at a fatal level
+//
+// This bails out of execution
 func Fatal(err error) {
 	if err != nil {
 		Logger.Fatal(err)
 	}
 }
 
+// Fatalf is used to catch non-nil errors and Log them at a fatal level with a
+// custom message
+//
+// This bails out of execution
 func Fatalf(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Errorf(msg, args...)
@@ -78,12 +100,19 @@ func Fatalf(err error, msg string, args ...interface{}) {
 	}
 }
 
+// Panic is used to catch non-nil errors and Log them at a panic level
+//
+// This causes execution to panic when called
 func Panic(err error) {
 	if err != nil {
 		Logger.Panic(err)
 	}
 }
 
+// Panicf is used to catch non-nil errors and Log them at a panic level with a
+// custom message
+//
+// This causes execution to panic when called
 func Panicf(err error, msg string, args ...interface{}) {
 	if err != nil {
 		Logger.Errorf(msg, args...)
