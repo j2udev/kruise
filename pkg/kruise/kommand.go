3a4b73c018376a050796e6a091a3519fc1178c4f
Joshua Ward <joshua.l.ward@leidos.com>
2022-08-23 01:36:13 +0000

    cleaning up deployment profiles feature and updating readme

--- pkg/kruise/kommand.go
+++ pkg/kruise/kommand.go
@@ -301,7 +301,7 @@ func (k Kommand) HelpFunc() func(*cobra.Command, []string) {
 // options and other CLI parameters specific to Kruise
 func (k Kommand) UsageTemplate() string {
 	return `Usage:{{if .Cmd.Runnable}}
-  {{.Cmd.UseLine}}{{end}} [options]{{if .Cmd.HasAvailableSubCommands}}
+  {{.Cmd.UseLine}}{{end}} [options]|[profiles]{{if .Cmd.HasAvailableSubCommands}}
   {{.Cmd.CommandPath}} [command]{{end}}{{if gt (len .Cmd.Aliases) 0}}
 
 Aliases:
@@ -315,7 +315,7 @@ Options:{{range .Opts }}
 
 Profiles:{{range .Profiles }}
   {{.Args}}	{{.Desc}}
-    |- Options:	{{range .Items}}{{.}} {{end}}{{end}}{{if .Cmd.HasAvailableLocalFlags}}
+    └── Options:	{{range .Items}}{{.}} {{end}}{{end}}{{if .Cmd.HasAvailableLocalFlags}}
 
 Flags:
 {{.Cmd.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .Cmd.HasAvailableInheritedFlags}}
